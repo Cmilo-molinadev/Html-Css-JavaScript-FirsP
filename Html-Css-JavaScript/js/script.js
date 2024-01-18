9d303670b2752eb117e5813be9e7051dce92ba16
Cmilo-molinadev <brayancamilomolinadev@gmail.com>
2024-01-18 08:20:01 -0500

    Edit somthings in HTML and i add the creature enemy with manipulation Of DOM  ¡its great!

--- Html-Css-JavaScript/js/script.js
+++ Html-Css-JavaScript/js/script.js
@@ -2,7 +2,7 @@
 function starGame(){
     let selectCreature = document.getElementById('Select-a-creauture');
     selectCreature.addEventListener('click', selectCreaturePj);
-}
+};
 window.addEventListener('load', starGame);
 
 //Function select creature 
@@ -15,7 +15,7 @@ function selectCreaturePj(){
     let octopuset = document.getElementById('octopuset');
     let talltree = document.getElementById('talltree');
     let mrfigth = document.getElementById('mrfigth');
-    let creatureSelected = document.getElementById('creature-pj')
+    let creatureSelected = document.getElementById('creature-pj');
 
     if (electrion.checked){
         creatureSelected.innerHTML = 'Electrion'; 
@@ -34,9 +34,36 @@ function selectCreaturePj(){
     }else if (mrfigth.checked){
         creatureSelected.innerHTML = 'Mr. Figth'; 
     }else{
-        alert('Please select a creature')
+        alert('Please select a creature');
     };
+    creaturePC();
 }; 
 
+//PC Select the aleatorie creaute 
+function creaturePC(){
+    let ataleatorieAttack = selecCeaturePc(1, 8);
+    let creatureSelectedPc = document.getElementById('creture-pc');
+     // DOM manipulation with innerHTML
+    if (ataleatorieAttack == 1){
+        creatureSelectedPc.innerHTML ='Electrion';
+    }else if (ataleatorieAttack == 2){
+        creatureSelectedPc.innerHTML ='Snowda';
+    }else if(ataleatorieAttack == 3){
+        creatureSelectedPc.innerHTML ='Firecat';
+    }else if (ataleatorieAttack == 4){
+        creatureSelectedPc.innerHTML ='Fairy';
+    }else if(ataleatorieAttack == 5){
+        creatureSelectedPc.innerHTML ='Darkghost';
+    }else if(ataleatorieAttack == 6){
+        creatureSelectedPc.innerHTML ='Octopused';
+    }else if(ataleatorieAttack == 7){
+        creatureSelectedPc.innerHTML ='Talltree';
+    }else{
+        creatureSelectedPc.innerHTML ='Mr. Figth';
+    };
 
+};
+function selecCeaturePc(min, max){
+    return Math.floor(Math.random()*(max - min + 1) + min);
+};
 
